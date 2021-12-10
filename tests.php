d78e7f0580e45a6b1aa2edf6fa303c275ff21a45
Lapique <lena_s82a@mail.ru>
2021-12-10 18:16:28 +0300

    String repeat

--- tests.php
+++ tests.php
@@ -3,6 +3,20 @@
 function litres($t) {
   return $t > 0 ? floor($t / 2) : 0;
   }
-  $time=11.8;
-  //time = 3 ----> litres = 1
-  echo 'time = ' . $time  . ' ----> litres = ' . litres($time);
+
+  $time=11.8;  
+  echo 'time = ' . $time  . ' ----> litres = ' . litres($time) . '<br>';
+
+  
+ // String repeat
+ function repeatStr($n, $str)
+{
+  $strMain = '';
+  if ($n>0){
+    for ($i=0; $i<$n; $i++) {
+      $strMain .= $str;
+    }
+  } 
+  return $strMain;
+}
+echo  repeatStr(6, "7"). '<br>';;
