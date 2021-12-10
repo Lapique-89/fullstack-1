5f265200f51b81ba6af55c3be1c61cd780897d9e
Lapique <lena_s82a@mail.ru>
2021-12-10 19:35:32 +0300

    еще три

--- tests.php
+++ tests.php
@@ -19,4 +19,35 @@ function litres($t) {
   } 
   return $strMain;
 }
-echo  repeatStr(6, "7"). '<br>';;
+echo  repeatStr(6, "7") . '<br>';
+
+//Return the Nth Even Number - тестирование не прошла, не понимаю, в чем ошибка 'Failed asserting that null matches expected 2.'
+function nthEven($n) {
+  if ($n == 0) {
+    echo 'The input will not be 0';
+  } else if ($n == 2){
+    echo $n . ' --> ' . 1;
+  } else {
+    echo $n . ' --> ' . (($n - 1) * 2);
+  }    
+}
+$n = 4;
+nthEven($n);
+echo '<br>';
+
+//Complete the solution so that it reverses the string passed into it.
+function solution($str) {
+  return strrev((string) $str);
+}
+$str = 1234;
+echo $str . ' => ' . solution($str) . '<br>';
+
+//Thinkful - String Drills: Quotable
+function quotable($name, $quote) {
+  $str = $name . ' said: "' . $quote. '"' ;
+  return (string) $str;
+}
+$name = 'Grae';
+$quote = 'Practice makes perfect'; 
+echo quotable($name, $quote) . '<br>';
+
