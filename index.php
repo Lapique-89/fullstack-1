5f0195d7e251887fd172cb6b61e43f3e75ebc301
Lapique <lena_s82a@mail.ru>
2021-12-24 18:13:08 +0300

    Обновление пользователя

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <!doctype html>
+<!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    <style>
+        .btn-danger {
+            border-radius: 100px;
+            padding: 2px 12px 4px 12px;
+        }
+    </style>
   </head>
   <body>
-  <div class='container mt-5'>
-       
-<pre>
+    <div class='container mt-5'>
 <?php
 $user =  'root';
 $password ='';
